5ae504ee9ade932466cc0f66599716fb6c36ca1b
Kyung Hun <pkh543@naver.com>
2022-03-28 16:50:28 +0900

    [Silver V] Title: 단어 정렬, Time: 488 ms, Memory: 27008 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            list.add(br.readLine());
+        }
+
+        list = list.stream().distinct().sorted().sorted((o1, o2) -> o1.length() - o2.length()).collect(Collectors.toList());
+
+        for (String i : list) {
+            if (!i.isBlank()) {
+                sb.append(i).append("\n");
+            }
+        }
+        System.out.println(sb);
+
+    }
+
+}
