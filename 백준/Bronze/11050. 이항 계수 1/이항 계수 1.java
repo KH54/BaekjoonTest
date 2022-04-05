e280a5a36390c1acfe01a2d8128da09ea173de03
Kyung Hun <pkh543@naver.com>
2022-04-05 15:48:42 +0900

    [Bronze I] Title: 이항 계수 1, Time: 128 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        System.out.println(factorial(N) / (factorial(K) * factorial(N - K)));
+    }
+
+    public static int factorial(int num) {
+        int result = 1;
+
+        while (num != 0) {
+            result *= num;
+            num--;
+        }
+        return result;
+    }
+
+}
