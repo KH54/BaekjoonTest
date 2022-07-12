cae121f9ed19a200d70c1f200000c4df8757c913
Kyung Hun <pkh543@naver.com>
2022-07-12 17:26:00 +0900

    [Silver IV] Title: 스택, Time: 204 ms, Memory: 16484 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int n = 0; n < N; n++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            String command = st.nextToken();
+
+            int num = 0;
+
+            if (st.hasMoreTokens()) {
+                num = Integer.parseInt(st.nextToken());
+            }
+
+            switch (command) {
+            case "push":
+                stack.push(num);
+                break;
+
+            case "top":
+                if (stack.empty()) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(stack.peek()).append("\n");
+                }
+                break;
+
+            case "size":
+                sb.append(stack.size()).append("\n");
+                break;
+
+            case "empty":
+                if (stack.empty()) {
+                    sb.append(1).append("\n");
+                } else {
+                    sb.append(0).append("\n");
+                }
+                break;
+
+            case "pop":
+                if (stack.empty()) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(stack.pop()).append("\n");
+                }
+                break;
+            }
+        }
+        System.out.println(sb);
+    }
+
+}
