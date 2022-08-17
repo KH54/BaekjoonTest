aef16a11f03322a90dd02740e9a5f816ae193de8
Kyung Hun <pkh543@naver.com>
2022-08-17 16:02:28 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        int[] basic = { 1, 1, 2, 2, 2, 8 };
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 6; i++) {
+            int horse = Integer.parseInt(st.nextToken());
+
+            sb.append(basic[i] - horse).append(" ");
+
+        }
+        
+        System.out.println(sb);
+    }
+
+}
